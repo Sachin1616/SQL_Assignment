7ce2e32da22f82aed80e3432f9d27fd61d196630
Sachin1616 <128647066+Sachin1616@users.noreply.github.com>
2023-12-21 10:11:47 +0530

    Add files via upload

---
+++ Assignment/SQL TECH SHOP.sql
@@ -0,0 +1,437 @@
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                    --TASK-1  Database Design (Normalization)
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Q1.1
+CREATE DATABASE TechShop;
+USE TechShop;
+
+-- Q1.2
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(20),
+    Address VARCHAR(255)
+); 
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Description TEXT,
+    Price DECIMAL(10, 2)
+);
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    TotalAmount DECIMAL(10, 2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+CREATE TABLE Inventory (
+    InventoryID INT PRIMARY KEY,
+    ProductID INT,
+    QuantityInStock INT,
+    LastStockUpdate DATE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+-- Q1.3
+-- The Customers table is in 1NF, 2NF, and 3NF.
+-- The Products table is in 1NF, 2NF, and 3NF.
+-- The Orders table is in 1NF, 2NF, and 3NF.
+-- The OrderDetails table is in 1NF, 2NF, and 3NF.
+-- The Inventory table is in 1NF, 2NF, and 3NF.
+
+-- Q1.4 (This task is done in MS Paints)
+
+--Q1.5 
+-- This is created in Q1.2 by default.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                           --TASK-2  Data Definition Language (DDL):
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q2.1 
+-- Creating Customers table
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(20),
+    Address VARCHAR(255)
+);
+
+-- Creating Products table
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Description TEXT,
+    Price DECIMAL(10, 2)
+);
+
+-- Creating Orders table
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    TotalAmount DECIMAL(10, 2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+-- Creating OrderDetails table
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+-- Creating Inventory table
+CREATE TABLE Inventory (
+    InventoryID INT PRIMARY KEY,
+    ProductID INT,
+    QuantityInStock INT,
+    LastStockUpdate DATE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                     --TASK-3 Data Manipulation Language (DML)
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q3.a
+INSERT INTO Customers (CustomerID, FirstName, LastName, Email, Phone, Address) 
+VALUES 
+(1, 'John', 'Doe', 'john@example.com', '123-456-7890', '123 Main St'),
+(2, 'Jane', 'Smith', 'jane@example.com', '987-654-3210', '456 Elm St'),
+(3, 'Alice', 'Johnson', 'alice@example.com', '111-222-3333', '789 Oak Ave'),
+(4, 'Bob', 'Williams', 'bob@example.com', '444-555-6666', '101 Pine St'),
+(5, 'Emily', 'Brown', 'emily@example.com', '777-888-9999', '210 Cedar St'),
+(6, 'Michael', 'Davis', 'michael@example.com', '222-333-4444', '55 Birch Ln'),
+(7, 'Sophia', 'Martinez', 'sophia@example.com', '999-888-7777', '890 Walnut St'),
+(8, 'Olivia', 'Garcia', 'olivia@example.com', '666-555-4444', '345 Maple Ave'),
+(9, 'William', 'Rodriguez', 'william@example.com', '333-222-1111', '678 Oak St'),
+(10, 'Ethan', 'Lopez', 'ethan@example.com', '555-444-3333', '432 Pine Ave');
+
+INSERT INTO Products (ProductID, ProductName, Description, Price) 
+VALUES 
+(1, 'Laptop', 'High-performance laptop with SSD', 1200.00),
+(2, 'Smartphone', 'Latest smartphone model with advanced camera', 800.00),
+(3, 'Tablet', '10-inch tablet with long battery life', 500.00),
+(4, 'Headphones', 'Wireless noise-canceling headphones', 150.00),
+(5, 'Smartwatch', 'Fitness tracking smartwatch', 200.00),
+(6, 'Bluetooth Speaker', 'Portable Bluetooth speaker with excellent sound', 100.00),
+(7, 'Gaming Console', 'Powerful gaming console for immersive gaming', 400.00),
+(8, 'Camera', 'Digital camera with high-resolution sensor', 700.00),
+(9, 'Drone', 'Quadcopter drone with HD camera', 1000.00),
+(10, 'External Hard Drive', 'Portable external hard drive for data storage', 80.00);
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate, TotalAmount) 
+VALUES 
+(1, 3, '2023-11-01', 1500.00),
+(2, 6, '2023-11-03', 900.00),
+(3, 1, '2023-11-05', 300.00),
+(4, 8, '2023-11-07', 600.00),
+(5, 5, '2023-11-10', 400.00),
+(6, 10, '2023-11-12', 1100.00),
+(7, 2, '2023-11-15', 2000.00),
+(8, 7, '2023-11-18', 700.00),
+(9, 9, '2023-11-20', 250.00),
+(10, 4, '2023-11-22', 1000.00);
+
+INSERT INTO OrderDetails (OrderDetailID, OrderID, ProductID, Quantity) 
+VALUES 
+(1, 1, 1, 2),
+(2, 1, 3, 1),
+(3, 2, 2, 1),
+(4, 2, 5, 3),
+(5, 3, 4, 4),
+(6, 4, 6, 2),
+(7, 5, 8, 1),
+(8, 6, 9, 1),
+(9, 6, 10, 3),
+(10, 7, 7, 1);
+
+INSERT INTO Inventory (InventoryID, ProductID, QuantityInStock, LastStockUpdate) 
+VALUES 
+(1, 1, 15, '2023-11-01'),
+(2, 2, 20, '2023-11-01'),
+(3, 3, 10, '2023-11-01'),
+(4, 4, 30, '2023-11-01'),
+(5, 5, 25, '2023-11-01'),
+(6, 6, 18, '2023-11-01'),
+(7, 7, 12, '2023-11-01'),
+(8, 8, 8, '2023-11-01'),
+(9, 9, 5, '2023-11-01'),
+(10, 10, 22, '2023-11-01');
+
+SELECT*FROM Inventory 
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                      --TASK-3 Data Manipulation Language (DML)
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Q3.b.1
+SELECT FirstName, LastName, Email FROM Customers;
+
+--Q3.b.2
+SELECT Orders.OrderID, Orders.OrderDate, Customers.FirstName, Customers.LastName FROM Orders JOIN Customers
+ON Orders.CustomerID = Customers.CustomerID;
+
+--Q3.b.3
+INSERT INTO Customers (CustomerID, FirstName, LastName, Email, Phone, Address)
+VALUES ('11', 'Anish', 'kolapudi', 'Anish@gmail.com', '123-456-7890', '433 Pine Ave');
+
+--Q3.b.4
+UPDATE Products SET Price=Price*1.1
+WHERE Description LIKE '%High-performance laptop with SSD%'
+
+--Q3.b.5
+DECLARE @OrderIDToDelete INT; 
+SET @OrderIDToDelete = 1; 
+
+
+DELETE FROM OrderDetails
+WHERE OrderID = @OrderIDToDelete;
+
+
+DELETE FROM Orders
+WHERE OrderID = @OrderIDToDelete;
+
+--Q3.b.6
+INSERT INTO Orders (OrderID, CustomerID, OrderDate, TotalAmount) 
+VALUES (1, 3, '2023-11-01', 1500.00);
+
+--Q3.b.7
+UPDATE Customers
+SET Email = 'Deo@example.com', Address = '124 Main St'
+WHERE CustomerID = '1'; 
+
+--Q3.b.8
+UPDATE Orders
+SET TotalAmount=(
+	SELECT SUM(P.Price * OD.Quantity)
+	From OrderDetails AS OD
+	INNER JOIN Products AS P ON OD.ProductId = P.ProductId
+	WHERE OD.OrderId = Orders.OrderId
+);
+
+
+--Q3.b.9
+DECLARE @CustomerIDToDelete INT; 
+SET @CustomerIDToDelete = '3'; 
+DELETE FROM OrderDetails
+WHERE OrderID IN (
+    SELECT OrderID FROM Orders
+    WHERE CustomerID = @CustomerIDToDelete
+);
+DELETE FROM Orders
+WHERE CustomerID = @CustomerIDToDelete;
+
+--Q3.b.10
+INSERT INTO Products (ProductID, ProductName, Description, Price)
+VALUES ('11', 'Earphones', 'Description of the new gadget', 399.99);
+
+--Q3.b.11
+ALTER TABLE Orders
+ADD Status VARCHAR(20);
+
+DECLARE @OrderIdToUpdate INT;
+DECLARE @newStatus VARCHAR(20);
+
+SET @OrderIdToUpdate = 5;
+SET @newStatus = 'Shipped';
+
+UPDATE Orders
+SET Status = @newStatus
+Where OrderId = @OrderIdToUpdate;
+
+--Q3.b.12
+DECLARE @NumberOfOrders INT
+UPDATE Customers
+SET @NumberOfOrders = (
+    SELECT COUNT(OrderID) FROM Orders
+    WHERE Customers.CustomerID = Orders.CustomerID);
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                           --TASK-4 JOINS
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q4.1
+SELECT Orders.OrderID, Orders.OrderDate, Orders.TotalAmount, Customers.FirstName , Customers.LastName FROM Orders JOIN Customers
+ON Orders.CustomerID = Customers.CustomerID;
+
+--Q4.2
+SELECT Products.ProductID, Products.ProductName, SUM(OrderDetails.Quantity * Products.Price) AS TotalRevenue FROM Products
+JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID
+JOIN Orders ON OrderDetails.OrderID = Orders.OrderID
+GROUP BY Products.ProductID, Products.ProductName
+ORDER BY TotalRevenue DESC;
+
+--Q4.3
+SELECT DISTINCT C.FirstName, C.LastName, C.Email, C.Phone, C.Address
+FROM Customers AS C
+JOIN Orders AS O ON C.CustomerID = O.CustomerID;
+
+--Q4.4
+SELECT P.ProductName, SUM(OD.Quantity) AS TotalQuantityOrdered
+FROM Products AS P
+JOIN OrderDetails AS OD ON P.ProductID = OD.ProductID
+GROUP BY P.ProductName 
+ORDER BY TotalQuantityOrdered DESC LIMIT 1;
+
+--Q4.5
+SELECT ProductName AS electronic_gadget, Category FROM Products
+WHERE Category IS NOT NULL;
+
+
+--Q4.6
+SELECT C.FirstName, C.LastName, AVG(OD.Quantity * P.Price) AS AverageOrderValue
+FROM Customers AS C
+JOIN Orders AS O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+GROUP BY C.CustomerID, C.FirstName, C.LastName;
+
+--Q4.7
+SELECT TOP 1 O.OrderID, C.FirstName, C.LastName, C.Email, C.Phone, C.Address, SUM(OD.Quantity * P.Price) AS TotalRevenue
+FROM Orders AS O
+JOIN Customers AS C ON O.CustomerID = C.CustomerID
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+GROUP BY O.OrderID, C.FirstName, C.LastName, C.Email, C.Phone, C.Address
+ORDER BY TotalRevenue DESC;
+
+
+--Q4.8
+SELECT P.ProductID, P.ProductName, COUNT(OD.OrderDetailID) AS TimesOrdered
+FROM Products AS P
+LEFT JOIN OrderDetails AS OD ON P.ProductID = OD.ProductID
+GROUP BY P.ProductID, P.ProductName
+ORDER BY TimesOrdered DESC;
+
+--Q4.9
+SELECT DISTINCT C.CustomerID, C.FirstName, C.LastName, C.Email, C.Phone, C.Address
+FROM Customers AS C
+JOIN Orders AS O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+WHERE P.ProductName = 'Headphones';
+
+--Q4.10
+SELECT SUM(OD.Quantity * P.Price) AS TotalRevenue
+FROM Orders AS O
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+WHERE O.OrderDate >= '2023-11-01' AND O.OrderDate <= '2023-11-03';
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                              -- TASK-5 Aggregate Functions 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q5.1
+SELECT C.CustomerID, C.FirstName, C.LastName, C.Email, C.Phone, C.Address
+FROM Customers AS C
+LEFT JOIN Orders AS O ON C.CustomerID = O.CustomerID
+WHERE O.OrderID IS NULL;
+
+--Q5.2
+SELECT COUNT(*) AS TotalProducts
+FROM Products;
+
+--Q5.3
+SELECT SUM(OD.Quantity * P.Price) AS TotalRevenue
+FROM Orders AS O
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID;
+
+--Q5.4
+-- THIS CANNOT BE SOLVED BECAUSE WE DON'T HAVE CATEGORY IN THE PROVIDED INPUT TABLE. BUT WE CAN USE ALTER AND ADD CATEGORY TABLE IN PRODUCTS.
+ALTER TABLE Products
+ADD Category VARCHAR(30);
+UPDATE Products
+SET Category = 'Electronics'
+WHERE ProductId IN(10,20,40,50,80)
+UPDATE Products
+SET Category = 'General'
+WHERE ProductId IN(30,60,70,80,100)
+DECLARE @InputCategoryName VARCHAR(255) = 'YourCategoryName';
+SELECT AVG(OD.Quantity) AS average_quantity_ordered FROM OrderDetails AS OD
+JOIN Products AS P ON OD.ProductId = P.ProductId 
+WHERE P.Category = @InputCategoryName;
+
+--Q5.5
+SELECT SUM(OD.Quantity * P.Price) AS TotalRevenue
+FROM Orders AS O
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+WHERE O.CustomerID = '5';
+
+--Q5.6
+SELECT C.FirstName, C.LastName, COUNT(O.OrderID) AS NumberOfOrders
+FROM Customers AS C
+LEFT JOIN Orders AS O ON C.CustomerID = O.CustomerID
+GROUP BY C.CustomerID, C.FirstName, C.LastName
+ORDER BY NumberOfOrders DESC;
+
+--Q5.7
+-- THIS CANNOT BE SLOVED BECAUSE WE DON'T HAVE INPUT CATEGORY.BUT WE CAN DO THIS IN THE BELOW STEPS.
+SELECT TOP 1 P.Category, SUM(OD.Quantity) AS total_quantity_ordered FROM OrderDetails AS OD
+JOIN Products AS P ON OD.ProductId = P.ProductId  
+GROUP BY P.Category
+ORDER BY total_quantity_ordered DESC;
+
+-- Q5.8
+SELECT C.FirstName, C.LastName, SUM(OD.Quantity * P.Price) AS TotalSpending
+FROM Customers AS C
+JOIN Orders AS O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+WHERE P.Description = 'Wireless noise-canceling headphones'
+GROUP BY C.CustomerID, C.FirstName, C.LastName
+ORDER BY TotalSpending DESC;
+
+--Q5.9
+SELECT DISTINCT C.FirstName, C.LastName, C.Email, C.Phone, C.Address
+FROM Customers AS C
+JOIN Orders AS O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+JOIN Products AS P ON OD.ProductID = P.ProductID
+WHERE P.ProductName = 'Headphones';
+
+
+--Q5.10
+SELECT C.FirstName, C.LastName, COUNT(O.OrderID) AS OrderCount
+FROM Customers AS C
+LEFT JOIN Orders AS O ON C.CustomerID = O.CustomerID
+GROUP BY C.CustomerID, C.FirstName, C.LastName
+ORDER BY OrderCount DESC;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment/SQLQuery SISDB.sql
@@ -0,0 +1,337 @@
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                     --TASK 1 Database Design (Normalization)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Q1 CREATE A TABLE NAMED SISDB.
+CREATE DATABASE SISDB;
+USE SISDB;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                     --TASK 2 Data Definition Language (DDL)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q2 DDL (CREATE TABLES)
+CREATE TABLE Students
+(
+student_id INT PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(40),
+date_of_birth DATE,
+email VARCHAR(50),
+phone_number VARCHAR(20)
+);
+
+CREATE TABLE Teacher
+(
+teacher_id INT PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+email VARCHAR(50)
+);
+
+CREATE TABLE Courses
+(
+course_id INT PRIMARY KEY,
+course_name VARCHAR(100),
+credits INT,
+teacher_id INT
+FOREIGN KEY(teacher_id) REFERENCES Teacher(teacher_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Enrollments
+(
+enrollment_id INT PRIMARY KEY,
+student_id INT FOREIGN KEY REFERENCES Students(student_id) ON DELETE CASCADE,
+course_id INT FOREIGN KEY REFERENCES Courses(course_id)ON DELETE CASCADE,
+enrollment_date DATE 
+);
+
+CREATE TABLE Payments
+(
+payment_id INT PRIMARY KEY,
+student_id INT FOREIGN KEY REFERENCES Students(student_id) ON DELETE CASCADE,
+amount FLOAT(2),
+payment_date DATE
+);
+SELECT*FROM Students
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                      --TASK 3 Data Manipulation Language (DML)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+---Q3.A DML INSERTING VALUES
+INSERT INTO Students 
+VALUES
+(1,'Sachin', 'Doe', '1995-08-15', 'john.doe@example.com', '1234567890'),
+(2,'Jeevan', 'Smith', '1998-03-22', 'alice.smith@example.com', '9876543210'),
+(3,'Chaitanya', 'Johnson', '1997-05-10', 'bob.johnson@example.com', '5551234567'),
+(4,'Krishna', 'lalth', '1996-11-03', 'emma.miller@example.com', '1237894560'),
+(5,'Ram', 'A', '1999-09-25', 'james.davis@example.com', '7890123456'),
+(6,'Seetha', 'V', '1994-04-18', 'olivia.white@example.com', '2345678901'),
+(7,'Lakshman', 'P', '1993-12-08', 'ethan.taylor@example.com', '4567890123'),
+(8,'Surekha', 'Balla', '1992-06-30', 'sophia.martin@example.com', '6789012345'),
+(9,'Nora', 'Fatima', '1991-02-14', 'noah.clark@example.com', '8901234567'),
+(10,'Divesh','Chinni','1993-11-05','divesh.chinni@gmail.com','1234567789');
+
+
+INSERT INTO Courses
+VALUES
+(1,'Mathematics', 3, 1),
+(2,'History', 4, 2),
+(3,'Computer Science', 5, 3),
+(4,'Physics', 4, 4),
+(5,'Literature', 3, 5),
+(6,'Chemistry', 4, 1),
+(7,'Biology', 4, 2),
+(8,'Art', 2, 3),
+(9,'Music', 2, 4),
+(10,'Physical Education', 2, 5);
+
+INSERT INTO Enrollments
+VALUES
+(001,2, 10, '2023-01-02'),
+(002,3, 9, '2023-01-03'),
+(003,4, 8, '2023-01-04'),
+(004,5, 7, '2023-01-05'),
+(005,6, 6, '2023-01-06'),
+(006,7, 5, '2023-01-07'),
+(007,8, 4, '2023-01-08'),
+(008,9, 3, '2023-01-09'),
+(009,10, 2, '2023-01-10'),
+(010,1, 1, '2023-01-01');
+
+
+
+INSERT INTO Teacher
+VALUES
+(1,'Mr.', 'Anderson', 'mr.anderson@example.com'),
+(2,'Mrs.', 'Johnson', 'mrs.johnson@example.com'),
+(3,'Dr.', 'Smith', 'dr.smith@example.com'),
+(4,'Ms.', 'Davis', 'ms.davis@example.com'),
+(5,'Prof.', 'Miller', 'prof.miller@example.com'),
+(6,'Miss', 'Taylor', 'miss.taylor@example.com'),
+(7,'Mr.', 'White', 'mr.white@example.com'),
+(8,'Mrs.', 'Clark', 'mrs.clark@example.com'),
+(9,'Dr.', 'Martin', 'dr.martin@example.com'),
+(10,'Ms.', 'Brown', 'ms.brown@example.com');
+
+INSERT INTO Payments
+VALUES
+(011,1, 500.00, '2023-02-01'),
+(012,2, 750.00, '2023-02-02'),
+(013,3, 600.00, '2023-02-03'),
+(014,4, 700.00, '2023-02-04'),
+(015,5, 550.00, '2023-02-05'),
+(016,6, 800.00, '2023-02-06'),
+(017,7, 900.00, '2023-02-07'),
+(018,8, 650.00, '2023-02-08'),
+(019,9, 720.00, '2023-02-09'),
+(110,10, 850.00, '2023-02-10');
+
+--Q3.B.1
+INSERT INTO Students (student_id, first_name, last_name, date_of_birth, email, phone_number)
+VALUES (11, 'John', 'Doe', '1995-08-15', 'john.doe@example.com', '1234567890');
+
+--Q3.B.2 (The **curdate** is a function used to get a current date of the enrollment date.)
+INSERT INTO Enrollments (enrollment_id, student_id, course_id, enrollment_date)
+VALUES (011, 1, 6, CURDATE());
+
+--Q3.B.3
+UPDATE Teacher
+SET email = 'new.email@example.com'
+WHERE teacher_id = 3;
+
+--Q3.B.4
+DELETE FROM Enrollments
+WHERE student_id = 2 AND course_id = 10;
+
+--Q3.B.5
+UPDATE Courses
+SET teacher_id = 3
+WHERE course_id = 3;
+
+--Q3.B.6
+SELECT * FROM Students WHERE student_id = 3; 
+DELETE FROM Enrollments WHERE student_id = 3;
+DELETE FROM Students WHERE student_id = 3;
+
+--Q3.B.7
+UPDATE Payments
+SET amount = 750.00
+WHERE payment_id = 014;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                           --TASK 4   JOINS
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q1
+SELECT Students.student_id, Students.first_name, Students.last_name, SUM(Payments.amount) AS total_payments
+FROM Students 
+JOIN Payments ON Students.student_id = Payments.student_id
+WHERE Students.student_id = 1
+GROUP BY Students.student_id, Students.first_name, Students.last_name;
+
+--Q2
+SELECT Courses.course_id, Courses.course_name, COUNT(Enrollments.student_id) AS enrolled_students
+FROM Courses 
+LEFT JOIN Enrollments ON Courses.course_id = Enrollments.course_id
+GROUP BY Courses.course_id, Courses.course_name
+ORDER BY Courses.course_id;
+
+--Q3
+SELECT Students.first_name, Students.last_name
+FROM Students
+LEFT JOIN Enrollments ON Students.student_id = Enrollments.student_id
+WHERE Enrollments.student_id IS NULL;
+
+--Q4
+SELECT Students.first_name, Students.last_name, Courses.course_name
+FROM Students 
+JOIN Enrollments  ON Students.student_id = Enrollments.student_id
+JOIN Courses  ON Enrollments.course_id = Courses.course_id;
+
+--Q5
+SELECT Teacher.first_name, Teacher.last_name, Courses.course_name
+FROM Teacher 
+INNER JOIN Courses ON Teacher.teacher_id = Courses.teacher_id;
+
+--Q6
+SELECT Students.first_name, Students.last_name, Enrollments.enrollment_date
+FROM Students 
+JOIN Enrollments  ON Students.student_id = Enrollments.student_id
+JOIN Courses  ON Enrollments.course_id = Courses.course_id
+WHERE Courses.course_name = 'Computer Science';
+
+--Q7
+SELECT Students.first_name, Students.last_name
+FROM Students
+LEFT JOIN Payments ON Students.student_id = Payments.student_id
+WHERE Payments.payment_id IS NULL;
+
+--Q8
+SELECT Courses.course_id, Courses.course_name
+FROM Courses 
+LEFT JOIN Enrollments ON Courses.course_id = Enrollments.course_id
+WHERE Enrollments.course_id IS NULL;
+
+--Q9
+SELECT student_id, COUNT(course_id) AS num_enrollments
+FROM Enrollments
+GROUP BY student_id
+HAVING COUNT(course_id) > 1;
+
+--Q10
+SELECT Teacher.teacher_id,Teacher.first_name,Teacher.last_name
+FROM Teacher 
+LEFT JOIN Courses ON Teacher.teacher_id = Courses.teacher_id
+WHERE Courses.teacher_id IS NULL;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+														--TASK 5 AGGREGATE FUNCTIONS AND SUBQUERIES 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Q1
+SELECT  Courses.course_name,
+    (
+        SELECT AVG(enrollment_count) 
+        FROM (
+            SELECT COUNT(*) AS enrollment_count
+            FROM Enrollments 
+            WHERE Enrollments.course_id = Courses.course_id
+            GROUP BY Enrollments.course_id
+        ) AS subquery
+    ) AS avg_students_enrolled
+FROM Courses ;
+
+--Q2
+SELECT Students.student_id, Students.first_name, Students.last_name, Payments.amount
+FROM Students
+JOIN Payments ON Students.student_id = Payments.student_id
+WHERE Payments.amount = (
+    SELECT MAX(amount)
+    FROM Payments);
+
+--Q3
+SELECT  course_name, COUNT(E.enrollment_id) FROM Courses AS C
+JOIN Enrollments AS E ON C.course_id = E.course_id
+GROUP BY course_name;
+
+--Q4
+SELECT Teacher.teacher_id, Teacher.first_name, Teacher.last_name, SUM(Payments.Amount) AS TotalPayments
+FROM Teacher 
+JOIN Courses ON Teacher.teacher_id = Courses.teacher_id
+JOIN Enrollments ON Courses.course_id = Enrollments.course_id
+JOIN Payments  ON Enrollments.student_id = Payments.student_id
+GROUP BY Teacher.teacher_id, Teacher.first_name, Teacher.last_name;
+
+--Q5
+SELECT student_id, first_name, last_name FROM Students
+WHERE (
+        SELECT COUNT(DISTINCT course_id) FROM Enrollments
+        ) = (
+        SELECT  COUNT(DISTINCT course_id) FROM Enrollments e
+        WHERE Students.student_id = e.student_id
+);
+
+--Q6
+SELECT Teacher_id, first_name, last_name FROM Teacher
+WHERE teacher_id NOT IN
+(
+	SELECT Teacher.teacher_id FROM Teacher 
+	JOIN Courses ON Teacher.teacher_id  = Courses.teacher_id 
+);
+
+--Q7
+SELECT StudentAges.student_id, AVG(StudentAge) AS AverageAge 
+FROM (
+	SELECT student_id, DATEDIFF(YEAR, date_of_birth, GETDATE()) AS StudentAge FROM Students
+) As StudentAges
+GROUP BY StudentAges.student_id;
+
+--Q8
+SELECT course_id, course_name FROM Courses
+WHERE course_id NOT IN(
+	SELECT Courses.course_id FROM Courses 
+	JOIN Enrollments ON Courses.course_id = Enrollments.course_id
+);
+
+--Q9
+SELECT Students.first_name, Students.last_name, SUM(Amount) AS TotalPayments FROM Students 
+JOIN Enrollments  ON Students.student_id = Enrollments.student_id
+JOIN Courses  ON Enrollments.course_id = Courses.course_id
+JOIN Payments  ON Enrollments.student_id = Payments.student_id
+GROUP BY Students.first_name, Students.last_name;
+
+--Q10
+SELECT Students.first_name, Students.last_name FROM Students 
+WHERE student_id IN (
+	SELECT student_id From Payments
+	GROUP BY student_id
+	HAVING COUNT(*)>1
+);
+
+--Q11
+SELECT Students.first_name, Students.last_name, SUM(Amount) AS TotalPayments FROM Students 
+JOIN Payments ON Students.student_id = Payments.student_id
+GROUP BY Students.first_name, Students.last_name;
+
+--Q12
+SELECT course_name, COUNT(enrollment_id) FROM Courses 
+JOIN Enrollments ON Courses.course_id = Enrollments.course_id
+GROUP BY course_name;
+
+--Q13
+SELECT  Students.first_name, Students.last_name, AVG(Amount) From Students
+JOIN Payments ON Students.student_id = Payments.student_id
+GROUP BY Students.first_name, Students.last_name;
+
+
+
+
+
+
+
+
+
+
